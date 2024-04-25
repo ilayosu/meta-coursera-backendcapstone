a41b182b382bdc6d1ba552e40308ffb917a08202
Illya Manin <ilay.manin@gmail.com>
2024-04-24 20:31:10 -0700

    added models, connected mysql, added test data to those models through created superuser

--- littlelemoncapstoneapp/models.py
+++ littlelemoncapstoneapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.core.validators import MaxValueValidator
 
 # Create your models here.
+class Booking(models.Model):
+    name = models.CharField(max_length=255)
+    guest_amount = models.SmallIntegerField(validators = [MaxValueValidator(6)])
+    booking_time = models.DateTimeField()
+    
+    def __str__(self):
+        return self.name + " - " + str(self.booking_time)
+
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10,decimal_places=2)
+    quantity = models.SmallIntegerField(validators = [MaxValueValidator(5)])
+
+    def __str__(self):
+        return self.title
