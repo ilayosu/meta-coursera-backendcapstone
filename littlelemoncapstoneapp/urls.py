be3d87605080d21b2883bf573b865e3237a7366d
Illya Manin <ilay.manin@gmail.com>
2024-04-26 09:02:58 -0700

    added views for Menu

--- littlelemoncapstoneapp/urls.py
+++ littlelemoncapstoneapp/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('menu', views.MenuItemView.as_view()),
+    path('menu/<int:pk>', views.SingleMenuItemView.as_view()),
 ]
